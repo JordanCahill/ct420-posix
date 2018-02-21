be6279b2191c7fc2e370f0fa29b4a30d4801b66e
jordancahill <jordancahill69@gmail.com>
2018-02-21 08:56:45 -0800

    Initial commit

---
+++ timer_usleep.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    struct timeval tv;
+    struct timezone tz;
+    int i, delay, num_iter, pid, priority;
+    double init, start, stop;
+
+    if (argc < 3) { // Case where user doesn't enter enough arguments
+        fprintf(stderr, "Usage: %s <sleep time..msec> <num_iteration>\n", argv[0]); 
+        exit(1);
+    }
+
+    // progname=argv[0];
+    delay = atoi(argv[1]); // parse arg 1 as an int
+    num_iter = atoi(argv[2]); // parse arg 2 as an int
+    pid = getpid(); // get priority id
+    priority = getpriority(PRIO_PROCESS, pid); // use pid to get priority
+
+    // initial time ref for the actual run-time
+    gettimeofday(&tv, &tz);
+    init = tv.tv_sec + tv.tv_usec * 0.000001;
+
+    for (i = 0; i < num_iter; ++i) {
+
+	// get start time
+        gettimeofday(&tv, &tz);
+        start = tv.tv_sec + tv.tv_usec * 0.000001;
+
+        // Now sleep
+        usleep(delay * 1000);
+
+	// get stop time
+        gettimeofday( &tv, &tz);
+        stop = tv.tv_sec + tv.tv_usec * 0.000001;
+
+    }
+
+    if(argc == 4) {
+        // Provides an option to print the CSV 
+        // actual_time, expected_time, pid, niceness, delay, iterations
+        printf("%lf, %d, %d, %d, %d, %d\n", (stop - init) * 1000, num_iter*delay, pid, priority, delay, num_iter);
+    }
+
+    printf("Total time taken: actual %lf theory (excl. runtime): %d ms, %d \n", (stop - init) * 1000, num_iter * delay, getpid());
+    
+    return 0;
+}
